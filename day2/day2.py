8a10ef21bda696fdc16241a905fdc0b9293ac960
Robin Elrharbi-fleury <relrharbifle@bloomberg.net>
2017-12-04 11:47:35 -0500

    day2 part2

--- day2/day2.py
+++ day2/day2.py
@@ -1,11 +1,36 @@
+#!/opt/bb/bin/python
+
 import sys
 
 sum = 0
 
+grid = []
 for line in sys.stdin:
     values = []
     for var in line.split():
         values.append(int(var))
+    grid.append(values)
     sum += max(values) - min(values)
 
+print("First Part")
+print(sum)
+
+sum = 0
+
+def divide(x, values):
+    for v in values:
+        m = max(x, v)
+        n = min(x, v)
+        if m % n == 0:
+            return m / n
+    return -1
+
+for line in grid:
+    val = 0
+    for i in range(len(line)):
+        res = divide(line[i], line[i + 1:])
+        if res != -1: break
+    sum += res
+
+print("Second Part")
 print(sum)
