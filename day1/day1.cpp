f7a33ff4b85607c88c7a1840eb88e6e88cdb2bab
Robin Elrharbi-fleury <relrharbifle@bloomberg.net>
2017-12-04 11:34:54 -0500

    day1 part2

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -7,15 +7,30 @@ int main()
     string input;
     cin >> input;
 
+    // First Part
+    cout << "First Part:\n";
     int sum = 0;
     int range = 0;
-
+    int size  = input.size();
     for (const char&  c : input) {
-         if (c == input[++range % input.size()]) {
+         if (c == input[++range % size]) {
              sum += c - '0';
          }
     }
+    cout << sum << endl;
 
+    // Second Part
+    cout << "Second Part:\n";
+    sum   = 0;
+    range = 0;
+    int i = 0;
+    for (const char&  c : input) {
+        range = i + size / 2;
+        i++;
+        if (c == input[range % size]) {
+            sum += c - '0';
+        }
+    }
     cout << sum << endl;
     return 0;
 }
